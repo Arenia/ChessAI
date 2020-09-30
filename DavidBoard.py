377242bcfd7375ff4fd69b9623a1df38eefee3ef
deyerLast <meyer.david221@gmail.com>
2020-09-30 14:40:18 -0600

    Board

--- DavidBoard.py
+++ DavidBoard.py
@@ -6,7 +6,8 @@ Thnking use this for user input as well as the board>?? idk.
 '''
 
 import pygame as p
-from ChessAI import DavidChessEngine
+#from ChessAI import DavidChessEngine
+import DavidChessEngine
 
 p.init()
 WIDTH = HEIGHT = 512
@@ -20,9 +21,10 @@ Initialize a global dictionary of images. this will be call exacly once in main
 '''
 def loadImages():
     #IMAGES['wp'] = p.image.load("Images/wp.png") #from folder images / 'the picture to use for that piece.'
-	#IMAGES['bp'] = p.image.load("Images/bp.png")
-	pieces = ['wp','wR','wN','wB','wK','wQ', 'bp','bR','bN','bB','bK','bQ'] 
-	IMAGES[pieces] = p.image.load("Images/" + pieces + ".png")
+    #IMAGES['bp'] = p.image.load("Images/bp.png")
+    pieces = ['wp','wR','wN','wB','wK','wQ', 'bp','bR','bN','bB','bK','bQ']
+    for piece in pieces:
+        IMAGES[piece] = (p.image.load("Images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
 '''
 Main driver for code. Handle user Input and updating the graphics :)
@@ -31,18 +33,18 @@ Main driver for code. Handle user Input and updating the graphics :)
 
 def main():
     p.init()
-	screen = p.display.set_mode((WIDTH, HEIGHT))
-	clock = p.time.Clock()
-	screen.fill(p.Color("white"))
-	gs = DavidChessEngine.GameState()#calling my ChessEngine for the state of the board
-	loadImages() #only do this once before the while loop :)
-	running = True
-	while running:
-	    for e in p.event.get():
-		    if e.type == p.QUIT:
-			    running = False
-		clock.tick(MAX_FPS)
-		p.display.flip()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = DavidChessEngine.GameState()#calling my ChessEngine for the state of the board
+    loadImages() #only do this once before the while loop :)
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        clock.tick(MAX_FPS)
+        p.display.flip()
 
 
 '''
@@ -50,29 +52,29 @@ Responsible for all graphics in game
 '''
 def drawGameState(screen, gs): #this draws the squares on the board
     drawBoard(screen)
-	#add piece highlighting or move suggestions (later?)
-	drawPieces(screen, gs.board)#draw pieces on the squares
+    #add piece highlighting or move suggestions (later?)
+    drawPieces(screen, gs.board)#draw pieces on the squares
 
 '''
 Draw squares on board
 '''
 def drawBoard(screen):
     colors = [p.Color("white"), p.Color("gray")]
-	for r in range(DIMENSION):
-	    for c in range(DIMENSION):
-		    color = colors[((r+c)%2)]
-			p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE)) 
-			#Don't draw pieces here just incase we want to implement highlighting
-			#Extra loop isn't that expensive 
-''''
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE)) 
+            #Don't draw pieces here just incase we want to implement highlighting
+            #Extra loop isn't that expensive 
+'''
 Draw pieces on board using current GameState.board
-''''
+'''
 def drawPieces(screen,board):
     for r in range(DIMENSION):
-	    for c in range(DIMENSION):
-		    piece = board[r][c]
-			if piece != "--": #not empty squares
-			    screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE)) #Puts the piece in for us
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #not empty squares
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE)) #Puts the piece in for us
 
 
 
